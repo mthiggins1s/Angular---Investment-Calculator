a2dfde46eaf83d630e891112b4661ccf0dba4b4b
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-31 11:22:45 -0500

    first commit

---
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+ @Output() calculate = new EventEmitter<{
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number,
+}>();
+enteredInitialInvestment = '0'; // property being binded to ngModel. (in the html).
+enteredAnnualInvestment = '0'; // property being binded to ngModel. (in the html).
+enteredExpectedReturn = '5'; // property being binded to ngModel. (in the html).
+enteredDuration = '10'; // property being binded to ngModel. (in the html).
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: + this.enteredInitialInvestment,
+      duration: + this.enteredDuration,
+      expectedReturn: + this.enteredExpectedReturn,
+      annualInvestment: + this.enteredAnnualInvestment
+    }) // emit this no matter what. even if you create it or use the output function.
+  }
+}
