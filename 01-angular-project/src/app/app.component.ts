70bdebb95f91d5385280637f0e3323c542db36c0
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-01 14:15:32 -0500

    Add songs component with styling and integrate into main layout

--- 01-angular-project/src/app/app.component.ts
+++ 01-angular-project/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SongsComponent } from "./songs/songs.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [SongsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
