f36fdfbcc4c691feaae5d5f8aa090f402c18c3c3
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-02 12:00:31 -0500

    Add songs component with data binding and event handling

---
+++ 01-angular-project/src/app/songs-list.ts
@@ -0,0 +1,32 @@
+export const songs_list = [
+    {
+      id: 'u1',
+      name: 'Enter Sandman',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Nothing Else Matters',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Master of Puppets',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'The Unforgiven',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Fuel',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'For Whom The Bell Tolls',
+      avatar: 'user-6.jpg',
+    },
+  ];
--- 01-angular-project/src/app/songs/songs.component.ts
+++ 01-angular-project/src/app/songs/songs.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-songs',
+  standalone: true,
   imports: [],
   templateUrl: './songs.component.html',
   styleUrl: './songs.component.css'
 })
 export class SongsComponent {
-
-}
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) id!: string;
+ 
+   get imagePath() {
+     return 'assets/songs-covers/' + this.avatar
+   }
+ 
+   onSelectSong() {}
+ }
